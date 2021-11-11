757675a4181a77cd4f43531eac215af98685337a
Kyle Cho <kylejcho@gmail.com>
2021-11-11 16:31:24 -0500

    Basic Set up

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-let gridNum = 16; 
+let pixelNum = 16;
+let pixelCount = Math.pow(pixelNum,2) 
+console.log(pixelCount)
 
-let grid = document.createElement("div");
-grid.classList.add("grid");
-document.body.appendChild(grid);
+for (let i = 1; i <= pixelCount; i++) {
+    let createPixel = document.createElement("div");
+    createPixel.classList.add("pixel");
+    createPixel.id = "p" + i;
+    document.querySelector(".pixelContainer").appendChild(createPixel);
+}
 
-console.log("hi")
+for (let i = 1; i <= pixelCount; i++) {
+    let pixel = document.querySelector("#p" + i);
+    pixel.onmouseenter = function() {
+        this.style.backgroundColor= "black";
+    }
+}
